138839ead2e7c9c1def41a1ec258d7b5af03f67a
guk30000 <guk30000@mail.ru>
2024-04-24 22:13:48 +0500

    5.1 added

--- src/Main.java
+++ src/Main.java
@@ -117,5 +117,16 @@ public class Main {
             default:
                 break;
         }
+
+        // Задача 5.1
+        int monthNumber1 = 7;
+
+        switch (monthNumber1) {
+            case 1, 2, 12 -> System.out.println("Зима");
+            case 3, 4, 5 -> System.out.println("Весна");
+            case 6, 7, 8 -> System.out.println("Лето");
+            case 9, 10, 11 -> System.out.println("Осень");
+            default -> System.out.println("Такого месяца нет");
+        }
     }
 }
